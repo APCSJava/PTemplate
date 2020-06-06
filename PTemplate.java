0beca4bdfe174c0fa96e97c71c0b2328189d6cc6
Kent Collins <kent.r.collins@gmail.com>
2020-06-06 16:17:47 +0100

    added the PTemplate file

---
+++ PTemplate.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class PTemplate extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main(new String[] {"PTemplate"});
+	}
+	
+	public void settings() {}
+	
+	public void setup() {}
+	
+	public void draw() {}
+	
+	public void keyPressed() {}
+	
+	public void mousePressed() {}
+	
+
+}
